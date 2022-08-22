c31cf676dbfc12a845b044aba8f572ffd303e399
Mathys PIERREZ <mathyspierrez2@gmail.com>
2022-08-22 18:59:01 +0200

    Ajout de la coloration du bloc

--- Block.cs
+++ Block.cs
@@ -14,11 +14,18 @@ namespace _2048
     {
         private int valeur = 0;
         private bool moovable = false;
+
         public Block()
         {
             InitializeComponent();
             lblValeur.Text = valeur.ToString();
         }
+
+        public Label getLabel()
+        {
+            return lblValeur;
+        }
+
         public int Valeur
         {
             get
@@ -37,8 +44,20 @@ namespace _2048
                     this.Moovable = true;
                 }
                 lblValeur.Text = value.ToString();
+                switch (value)
+                {
+                    case 0: this.BackColor = Color.FromArgb(238, 228, 218); break;
+                    case 2: this.BackColor = ColorTranslator.FromHtml("#eee4da"); break;
+                    case 4: this.BackColor = ColorTranslator.FromHtml("#ede0c8"); break;
+                    case 8: case 16: this.BackColor = ColorTranslator.FromHtml("#f2b179"); break;
+                    case 32: this.BackColor = ColorTranslator.FromHtml("#f67c5f"); break;
+                    case 64: this.BackColor = ColorTranslator.FromHtml("#f65e3b"); break;
+                    default: this.BackColor = ColorTranslator.FromHtml("#edcf72"); break;
+                }
+                if (value > 4) lblValeur.ForeColor = ColorTranslator.FromHtml("#f9f6f2");
+                else lblValeur.ForeColor = ColorTranslator.FromHtml("#776e65");
+
                 lblValeur.Location = new Point((this.Width - lblValeur.Width) / 2, (this.Height - lblValeur.Height) / 2);
-                frmPartie.ColorBlockByValue(this, value);
             }
         }
 
